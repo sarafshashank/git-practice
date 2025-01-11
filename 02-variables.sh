51ea8a4a215eeb3df23757a9cce254e6794c0a5a
Shashank Saraf <saraf.shashank2001@gmail.com>
2025-01-11 01:14:33 -0500

    It is a script that explains about variables and it runs on DRY principle

---
+++ 02-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Author : Shashank Saraf
+# Date   : 2021-02-01
+# Purpose: This script is used to create a variables in the script.
+#          It is used to store the values which can be used later in the script.
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: Hi ${PERSON2}, How are you?"
+echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
+echo "$PERSON1:: I am doing good ${PERSON2}. What's going on?"
+echo "${PERSON2}:: I started learning Shell Script $PERSON1"
