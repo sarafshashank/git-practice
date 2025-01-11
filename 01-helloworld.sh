23711e0f7453f2144babe9a0b692ed54795ffeee
Shashank Saraf <saraf.shashank2001@gmail.com>
2025-01-11 00:54:41 -0500

    I added some documentation details to the file

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-echo "Hello world"
+# Author : Shashank Saraf
+# Date   : 2021-02-01
+# Purpose: This script is used to create a hello world file in the system.
+# Output : Hello World
+echo "Hello world"s
