d0547fceeb6ede0e0e0520eaf6d7c80ff08e13f4
Shashank Saraf <saraf.shashank2001@gmail.com>
2025-02-13 02:52:33 -0500

    Basic example of how arrays work

--- 05-array.sh
+++ 05-array.sh
@@ -11,15 +11,4 @@ echo "First fruit is: ${FRUITS[0]}"
 echo "Second fruit is: ${FRUITS[1]}"
 echo "Third fruit is: ${FRUITS[2]}"
 
-echo "First fruit is: ${FRUITS[@]}" # '@'  
-
-#!/bin/bash
-
-# index starts from 0, size is 3
-FRUITS=("APPLE" "KIWI" "ORANGE") #Array
-
-echo "First fruit is: ${FRUITS[0]}"
-echo "Second fruit is: ${FRUITS[1]}"
-echo "Third fruit is: ${FRUITS[2]}"
-
-echo "First fruit is: ${FRUITS[@]}"
+echo "First fruit is: ${FRUITS[@]}"  
