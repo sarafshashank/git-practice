57896085ab50657db9ec7d44980380fba41f3e34
Shashank Saraf <saraf.shashank2001@gmail.com>
2025-02-13 12:18:26 -0500

    This script shows basic use case of datatypes

---
+++ 06-data-types.sh
@@ -0,0 +1,15 @@
+# Author : Shashank Saraf
+# Date   : 07-02-2025
+# Purpose: This script demonstrates basic logic in datatypes
+
+#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "Total of $NUMBER1 and $NUMBER2 is: $SUM"
+
+echo "Product of $NUMBER1 and $NUMBER2 is: $(($NUMBER1*$NUMBER2))"
+echo "Difference of $NUMBER1 and $NUMBER2 is: $(($NUMBER1-$NUMBER2))"
