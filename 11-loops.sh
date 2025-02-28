c8fc236eb00508b659176c35a274c8d267514911
Shashank Saraf <saraf.shashank2001@gmail.com>
2025-02-28 02:11:41 -0500

    loops

---
+++ 11-loops.sh
@@ -0,0 +1,22 @@
+# Author : Shashank Saraf
+# Date   : 27-02-2025
+# Purpose: This script demonstrates Loops
+
+
+#!/bin/bash
+
+for i in {0..1000} #0-1000
+do
+	echo $i
+done
+
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+# echo "7"
+# echo "8"
+# echo "9"
+# echo "10"
